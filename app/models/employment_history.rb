67e5d6b32580ef1102db26c73a077da53237c513
kaalm1 <kaalm1@gmail.com>
2017-07-03 22:11:11 -0400

    date validation added

--- app/models/employment_history.rb
+++ app/models/employment_history.rb
@@ -1,4 +1,13 @@
 class EmploymentHistory < ActiveRecord::Base
   belongs_to :employer
   belongs_to :borrower
+
+  validate :start_date_before_end_date
+
+  def start_date_before_end_date
+    if start_date > end_date || (end_date.nil? && !start_date.nil?)
+      errors.add(:start_date, "is after end date")
+    end
+  end
+
 end
