36851cc8ad65ce99110bc27fa7357f7ca8e1dfaf
adames <adameshh@gmail.com>
2017-10-05 19:21:15 -0400

    added index page to borrowers

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -1,22 +1,12 @@
 class BorrowersController < ApplicationController
 
-  # TODO must split between routes and controller.
-
   def index
-    @borrower = Borrower.all
+    @borrowers = Borrower.all
   end
-
-  # get '/borrowers' do
-  #   erb :'borrowers/index.html'
-  # end
 
   def new
     @borrower = Borrower.new
   end
-
-  # get '/borrowers/new' do
-  #   erb :'borrowers/new.html'
-  # end
 
   def create
     #needs strong params integration
@@ -29,36 +19,15 @@ class BorrowersController < ApplicationController
     end
   end
 
-  # post '/borrowers' do
-  #   @borrower = Borrower.create(params[:borrower])
-  #   @borrower.card = Card.default_card
-  #   if @borrower.errors.any?
-  #     erb :'borrowers/new.html'
-  #   else
-  #     redirect_to "/borrowers/#{@borrower.id}"
-  #   end
-  # end
-
   def show
     redirect_to '/' if !Borrower.exists?(params[:id])
     @borrower = Borrower.find(params[:id])
   end
 
-  # get '/borrowers/:id' do
-  #   redirect_to '/' if !Borrower.exists?(params[:id])
-  #   @borrower = Borrower.find(params[:id])
-  #   erb :'borrowers/show.html'
-  # end
-
   def edit
     @borrower = Borrower.find(params[:id])
     erb :'borrowers/edit.html'
   end
-
-  # get '/borrowers/:id/edit' do
-  #   @borrower = Borrower.find(params[:id])
-  #   erb :'borrowers/edit.html'
-  # end
 
   def update
     @borrower = Borrower.find(params[:id])
@@ -66,21 +35,9 @@ class BorrowersController < ApplicationController
     redirect_to "/borrowers/#{@borrower.id}"
   end
 
-  # patch '/borrowers/:id' do
-  #   @borrower = Borrower.find(params[:id])
-  #   @borrower.update(params[:borrower])
-  #   redirect_to "/borrowers/#{@borrower.id}"
-  # end
-
   def delete
     Borrower.find(params[:id]).destroy
     redirect_to '/'
   end
-
-  # delete '/borrowers/:id' do
-  #   Borrower.find(params[:id]).destroy
-  #   redirect_to '/'
-  # end
-
 
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,29 +1,15 @@
 class StaticController < ApplicationController
 
-  # TODO must split between routes and controller.
-
   def about
     render 'static/about'
   end
-
-  # get '/about' do
-  #   erb :'static/about.html'
-  # end
 
   def careers
     render 'static/careers'
   end
 
-  # get '/careers' do
-  #   erb :'static/careers.html'
-  # end
-
   def contact
     render 'static/contact'
   end
-
-  # get '/contact' do
-  #   erb :'static/contact.html'
-  # end
 
 end
